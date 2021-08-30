046c6a437490c1c1e8c3a23c893af04d22db7c10
Jithin Balan <jithin.balan@arq.group>
2021-08-30 20:03:38 +1000

    Set root viewController

--- Source/Features/News/NewsListViewController.swift
+++ Source/Features/News/NewsListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsListViewController.swift
 //  JBNewsFeed
 //
 //  Created by Jithin Balan on 30/8/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
