1ec3fc5b1ab2d849da3b3d2ef82462dc8f57dd4d
Jithin Balan <jithin.balan@arq.group>
2021-08-31 13:03:31 +1000

    Add article view model

---
+++ Source/Features/News/ViewModel/ArticleViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleViewModel.swift
+//  JBNewsFeed
+//
+//  Created by Jithin Balan on 31/8/21.
+//
+
+import Foundation
+
+struct ArticleViewModel {
+    let title: String?
+    let author: String?
+    let publishedDate: String?
+    let excerpt: String?
+    let summary: String?
+    let mediaUrl: String?
+    
+    
+}
