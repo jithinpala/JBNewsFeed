95c08cfb744eed9da492db939aee015aa078ba21
Jithin Balan <jithin.balan@arq.group>
2021-08-31 14:22:19 +1000

    Add display protocol, update viewController and implement fetch news feed services

--- Source/Features/News/ViewController/NewsListViewController.swift
+++ Source/Features/News/ViewController/NewsListViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 
-class NewsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class NewsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NewsListDisplay {
     
     private let tableView = UITableView()
+    private var refreshControl: UIRefreshControl!
+    private lazy var viewModel = NewsListViewModel(display: self)
+    
+    private var activityIndicatorView = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupTableView()
+        setupView()
+        
+        viewModel.viewDidLoad()
+        addRefreshController()
     }
     
-    private func setupTableView() {
+    private func addRefreshController() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(pullToRefreshDidTrigger), for: .valueChanged)
+        tableView.addSubview(refreshControl)
+    }
+    
+    @objc func pullToRefreshDidTrigger() {
+        viewModel.pullToRefreshDidTrigger()
+    }
+    
+    private func setupView() {
+        title = viewModel.title
         view.addSubview(tableView)
+        view.addSubview(activityIndicatorView)
+        
+        activityIndicatorView.style = .gray
+        if #available(iOS 13.0, *) {
+            activityIndicatorView.style = .large
+        }
+        activityIndicatorView.hidesWhenStopped = true
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -35,22 +59,53 @@ class NewsListViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    // MARK: - NewsListDisplay
+    
+    func showLoadingIndicator() {
+        activityIndicatorView.startAnimating()
+    }
+    
+    func hideLoadingIndicator() {
+        activityIndicatorView.stopAnimating()
+    }
+    
+    func reloadDisplay() {
+        tableView.reloadData()
+        if refreshControl.isRefreshing {
+            stopPullToRefresh()
+        }
+    }
+    
+    func stopPullToRefresh() {
+        refreshControl.endRefreshing()
+    }
+    
+    func showErrorAlert(_ viewModel: DisplayErrorAlertViewModel) {
+        let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: viewModel.buttonTitle, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     // MARK: - UITableViewDataSource
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        viewModel.numberOfSections()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        viewModel.numberOfItems()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeue(NewsListViewCell.self, for: indexPath)
-        
-        cell.configure()
+        let articleViewModel = viewModel.item(at: indexPath)
+        cell.configure(articleViewModel)
         return cell
     }
 }
--- Source/Features/News/ViewModel/NewsListViewModel.swift
+++ Source/Features/News/ViewModel/NewsListViewModel.swift
@@ -7,11 +7,60 @@
 
 import Foundation
 
+protocol NewsListDisplay: AnyObject {
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+    func reloadDisplay()
+    func showErrorAlert(_ viewModel: DisplayErrorAlertViewModel)
+}
+
 final class NewsListViewModel {
+    private var articleViewModel = [ArticleViewModel]()
+    weak var display: NewsListDisplay?
+    var service: NewsFeedServicesProtocol
+    let title = "News Feed"
     
-    func viewDidLoad() {
-        
+    init(display: NewsListDisplay,
+         service: NewsFeedServicesProtocol = NewsFeedServices()) {
+        self.display = display
+        self.service = service
     }
     
+    func viewDidLoad() {
+        fetchNewsFeed(shouldShowIndicator: true)
+    }
     
+    func fetchNewsFeed(shouldShowIndicator: Bool) {
+        if shouldShowIndicator {
+            display?.showLoadingIndicator()
+        }
+        service.fetchNewsFeed { [weak self] result in
+            self?.display?.hideLoadingIndicator()
+            switch result {
+            case .success(let news):
+                self?.articleViewModel = news.articles?.map {
+                    ArticleViewModel(article: $0)
+                } ?? []
+                self?.display?.reloadDisplay()
+            case .failure(_):
+                self?.display?.showErrorAlert(DisplayErrorAlertViewModel())
+            }
+        }
+    }
+    
+    func pullToRefreshDidTrigger() {
+        fetchNewsFeed(shouldShowIndicator: false)
+    }
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfItems() -> Int {
+        articleViewModel.count
+    }
+    
+    func item(at indexPath: IndexPath) -> ArticleViewModel {
+        articleViewModel[indexPath.row]
+    }
 }
