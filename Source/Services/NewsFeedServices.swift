34a5481abbfb5def3b3d8356a06b785dcaeaf643
Jithin Balan <jithin.balan@arq.group>
2021-08-31 13:02:35 +1000

    Create News feed services

---
+++ Source/Services/NewsFeedServices.swift
@@ -0,0 +1,63 @@
+//
+//  NewsFeedServices.swift
+//  JBNewsFeed
+//
+//  Created by Jithin Balan on 31/8/21.
+//
+
+import Foundation
+
+typealias NewsFeedServicesResult = (Result <News, NSError>) -> Void
+
+final class NewsFeedServices {
+    
+    private enum QueryItem {
+        static let query = "q"
+        static let queryValue = "Tesla"
+        static let page = "page"
+        static let pageSize = "page_size"
+        static let pageSizeValue = "10"
+    }
+        
+    private let network = JBNetworkService()
+    
+    private var endPoint: String {
+        "https://api.newscatcherapi.com/v2/search"
+    }
+    
+    func fetchNewsFeed(completion: @escaping NewsFeedServicesResult) {
+        guard let request = makeRequest()
+        else { return }
+        network.genericDataTask(withRequest: request) { result in
+            switch result {
+            case let .success((_, data)):
+                do {
+                    let newsFeed = try JSONDecoder().decode(News.self, from: data)
+                    completion(.success(newsFeed))
+                } catch (let error) {
+                    print(error)
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+    
+    private func makeRequest() -> URLRequest? {
+        guard let url = newsFeedURL() else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        return request
+    }
+    
+    private func newsFeedURL() -> URL? {
+        guard var urlComponents = URLComponents(string: endPoint) else { return nil }
+        let queryItem = URLQueryItem(name: QueryItem.query, value: QueryItem.queryValue)
+        let pageItem = URLQueryItem(name: QueryItem.page, value: "1")
+        let pageSizeItem = URLQueryItem(name: QueryItem.pageSize, value: QueryItem.pageSizeValue)
+        urlComponents.queryItems = [queryItem, pageItem, pageSizeItem]
+        return urlComponents.url
+    }
+}
